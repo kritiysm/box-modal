2d58368e26e271c8ce74e18b6a2d1181fa4eee98
kriti singh <kritisingh7234@gmail.com>
2024-10-10 10:58:29 +0530

    change ther code task5.js

--- task5.js
+++ task5.js
@@ -1,3 +1,12 @@
-const addNumber = ([1,6,9,0,5])
-addNumber.unshift(0)
-console.log(addNumber)
+// const addNumber = ([1,6,9,0,5])
+// addNumber.unshift(0)
+// console.log(addNumber)
+
+function insertArray(array,variable){
+  let newArray = [variable];
+  for(let i = 0; i<array.length;i++){
+    newArray.push(array[i])
+  }
+  return newArray
+}
+console.log(insertArray([2,4,5,6,7],8))
